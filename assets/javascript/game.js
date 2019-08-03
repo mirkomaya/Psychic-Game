2a334c98ef76762bdb90c6f819a78ce75ab3e045
Marty McFly <mirko.maya@live.com>
2019-08-03 18:04:10 -0400

    Updated some html in doc

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,29 @@
 
 var computerLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var wins = 0;
 var losses = 0;
 var guesses = 10;
-var myArr = []
+var myArray = []
 
-var winsText = document.getElementById("wins-text"); 
+var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var remainText = document.getElementById("remain-text");
 var guessText = document.getElementById("guess-text");
 
 var computerGuess = computerLetter[Math.floor(Math.random() * computerLetter.length)];
-    console.log(computerGuess);
+console.log(computerGuess);
 
-document.onkeypress = function(event) {
+document.onkeypress = function (event) {
 
     var userGuess = event.key;
-    myArr.push(userGuess);
+    myArray.push(userGuess);
     if (userGuess === computerGuess) {
         wins++;
         guesses = 10;
         computerGuess = computerLetter[Math.floor(Math.random() * computerLetter.length)];
-        console.log(computerGuess);    
+        console.log(computerGuess);
 
     } else {
         guesses--;
@@ -31,14 +31,14 @@ document.onkeypress = function(event) {
     if (guesses === 0) {
         losses++;
         guesses = 10;
-        myArr = [];
+        myArray = [];
         computerGuess = computerLetter[Math.floor(Math.random() * computerLetter.length)];
         console.log(computerGuess);
-    } 
+    }
 
     winsText.textContent = "Wins: " + wins;
     lossesText.textContent = "Losses: " + losses;
     remainText.textContent = "Guesses Left: " + guesses;
-    guessText.textContent = "Your Guesses So Far: " + myArr.join(", ");
+    guessText.textContent = "Your Guesses So Far: " + myArray.join(", ");
 
 }
